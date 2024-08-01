6cc7a23c6c7d7ebe0459faeb30fa0bf41e943a9c
phucnh17 <phucnh17@msb.com.vn>
2024-08-02 00:48:52 +0700

    feat: init react query

--- app/verify/[id]/page.tsx
+++ app/verify/[id]/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import BasicInfor from "@/components/BasicInfor";
+import NotifyClient from "@/components/NotifyClient";
 
 export default function VerifyDetailPage() {
   return (
     <div>
       <BasicInfor />
+      <NotifyClient />
     </div>
   );
 }
