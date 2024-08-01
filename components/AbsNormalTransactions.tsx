eb783068483924c616f77a0b77aa8d9dc0b12bb1
phucnh17 <phucnh17@msb.com.vn>
2024-08-02 00:53:32 +0700

    feat: init react query

---
+++ components/AbsNormalTransactions.tsx
@@ -0,0 +1,186 @@
+import { doGet } from "@/utils/doMethod";
+import { formatCurrency } from "@/utils/format";
+import { FileProtectOutlined } from "@ant-design/icons";
+import { useQuery } from "@tanstack/react-query";
+import {
+  Button,
+  Col,
+  Pagination,
+  Row,
+  Table,
+  TableProps,
+  Tag,
+  Typography,
+} from "antd";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function AbsNormalTransactions() {
+  const param = useParams();
+  const router = useRouter();
+
+  const [transactions, setTransactions] = useState<any>([]);
+  const [transactionsTotal, setTransactionsTotal] = useState<number>(0);
+  const [transactionsPage, setTransactionsPage] = useState<number>(1);
+  const [tranLoading, setTranLoading] = useState<boolean>(false);
+
+  useQuery({
+    queryKey: ["get-transaction", [param.id, transactionsPage]],
+    queryFn: async () => {
+      setTranLoading(true);
+      const $filter = {
+        retrieverAccountId: param.id,
+      };
+      const _params = {
+        s: JSON.stringify($filter),
+        limit: 10,
+        page: transactionsPage,
+      };
+      const response = await doGet("/transaction-history", _params);
+      if (response?.statusCode === 200) {
+        setTransactions(response?.data?.data);
+        setTransactionsTotal(response?.data?.total);
+        setTransactionsPage(response?.data?.page);
+        setTranLoading(false);
+        return true;
+      }
+      setTranLoading(false);
+      return false;
+    },
+  });
+
+  const columns: TableProps["columns"] = [
+    {
+      title: "Tên tài khoản",
+      dataIndex: "senderAccountName",
+      key: "senderAccountName",
+    },
+    {
+      title: "Số tài khoản",
+      dataIndex: "senderAccountNumber",
+      key: "senderAccountNumber",
+    },
+    {
+      title: "Ngân hàng",
+      key: "senderBank",
+      render(value, record, index) {
+        return value?.senderBank?.name;
+      },
+    },
+    {
+      title: "Số tiền",
+      key: "amount",
+      dataIndex: "amount",
+      render(value, record, index) {
+        return formatCurrency(value);
+      },
+    },
+    {
+      title: "Trạng thái",
+      dataIndex: "status",
+      key: "status",
+      align: "center",
+      render(value, record, index) {
+        return <Tag color={`${getTagColor(value)}`}>{value}</Tag>;
+      },
+    },
+  ];
+
+  const getTagColor = (status: string) => {
+    return {
+      "Bảo lưu": "red",
+      "Đang xử lí": "orange",
+      "Đã giải ngân": "green",
+    }[status];
+  };
+
+  return (
+    <div className="py-3">
+      <Row>
+        <Col span={18}>
+          <Typography.Title level={5}>
+            Thông tin các giao dịch bất thường
+          </Typography.Title>
+        </Col>
+        <Col span={6}>
+          <Button
+            block
+            type="primary"
+            icon={<FileProtectOutlined />}
+            iconPosition="end"
+            onClick={() => {
+              router.push(`/verify/${param.id}`);
+            }}
+          >
+            Xác minh giao dịch
+          </Button>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col span={24}>
+          <div className="py-3">
+            <Table
+              loading={tranLoading}
+              pagination={false}
+              columns={columns}
+              dataSource={transactions}
+              bordered
+              scroll={{ x: 800, y: 500 }}
+            />
+          </div>
+
+          <div className="py-3">
+            <Pagination
+              align="center"
+              defaultCurrent={transactionsPage}
+              total={transactionsTotal}
+              pageSize={10}
+              onChange={(page) => setTransactionsPage(page)}
+            />
+          </div>
+        </Col>
+      </Row>
+
+      {/* <Modal
+          title="Xác minh giao dịch"
+          open={modalVerifyCode}
+          onCancel={() => setModalVerifyCode(false)}
+          footer={[
+            <Button
+              key="submit"
+              type="primary"
+              onClick={() => submitBtn?.current?.click()}
+              loading={verifyLoading}
+            >
+              Hoàn tất
+            </Button>,
+          ]}
+        >
+          <div className="py-3">
+            <Form
+              form={formVerifyCode}
+              autoComplete="off"
+              onFinish={onFinishVerifyCode}
+            >
+              <Form.Item
+                name="code"
+                rules={[
+                  { required: true, message: "Vui lòng nhập mã xác minh" },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+
+              <Button
+                ref={submitBtn}
+                type="primary"
+                htmlType="submit"
+                className="!hidden"
+              />
+            </Form>
+          </div>
+        </Modal> */}
+    </div>
+  );
+}
