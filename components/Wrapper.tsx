d1cf1c8741d8d1d1c42bff194046f3b67bda0697
phucnh17 <phucnh17@msb.com.vn>
2024-08-02 09:07:29 +0700

    feat: init react query

--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -3,21 +3,25 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Col, Layout, Row, Typography } from "antd";
 import { Content, Footer, Header } from "antd/es/layout/layout";
 import BgBanner from "@/assets/bg_banner.jpg";
+import { useRouter } from "next/navigation";
 
 const queryClient = new QueryClient();
 
 export default function Wrapper({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const router = useRouter();
+
   return (
     <QueryClientProvider client={queryClient}>
       <Layout className="">
         <Header
-          className="sticky top-0 z-10 w-full flex items-center !bg-white px-5 md:!px-40 bg-center bg-cover bg-no-repeat"
+          className="cursor-pointer sticky top-0 z-10 w-full flex items-center !bg-white px-5 md:!px-40 bg-center bg-cover bg-no-repeat"
           style={{ backgroundImage: `url(${BgBanner})` }}
+          onClick={() => router.push("/")}
         >
-          <Typography.Title level={5} className="!text-red-600 uppercase">
-            CỔNG THÔNG TIN ĐIỆN TỬ KHO BẠC NHÀ NƯỚC
+          <Typography.Title level={5} className="!text-red-600 uppercase !my-0">
+            CỔNG THÔNG TIN
           </Typography.Title>
         </Header>
 
