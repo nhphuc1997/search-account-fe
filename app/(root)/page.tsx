63c2907e55a5b5d14a56362008b7e1235020e774
phucnh17 <phucnh17@msb.com.vn>
2024-08-02 18:50:58 +0700

    feat: init react query

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Articles from "@/components/Article";
-import { Carousel, Col, Image, Row, Typography } from "antd";
+import { Carousel, Col, Divider, Row, Typography } from "antd";
 
 export default function IndexHomePage() {
   const images = [
@@ -11,19 +11,19 @@ export default function IndexHomePage() {
 
   return (
     <div className="w-full">
-      <Row gutter={16}>
+      <Row gutter={8}>
         <Col xs={24} md={20}>
           <div className="w-full">
-            <Row>
+            <Row className="">
               <Col xs={24} md={12}>
                 <Carousel effect="fade" dots={false} autoplay>
                   {images.map((item) => (
-                    <div>
+                    <div className="">
                       <div
-                        className="h-[300px] w-full bg-transparent bg-center bg-cover bg-no-repeat"
+                        className="h-[340px] w-full bg-transparent bg-center bg-cover bg-no-repeat"
                         style={{ backgroundImage: `url(${item})` }}
                       />
-                      <div className="mt-[-120px] bg-[#e9f0f6] opacity-75">
+                      <div className="mt-[-120px] bg-[#e9f0f6] opacity-75 px-2">
                         <Typography.Text className="text-base font-bold !text-[#2c6ea8]">
                           Kho bạc Nhà nước họp báo công bố kết quả thực hiện
                           nhiệm vụ trọng tâm trong 6 tháng đầu năm 2024
@@ -43,14 +43,14 @@ export default function IndexHomePage() {
                 </Carousel>
               </Col>
               <Col xs={24} md={12}>
-                <div className="h-[300px] overflow-hidden">
-                  <div className="bg-[#2573B5] p-1">
+                <div className="">
+                  <div className="bg-[#2573B5] px-3 py-1">
                     <Typography.Text className="text-sm font-medium !text-white uppercase">
                       Tin nổi bật
                     </Typography.Text>
                   </div>
 
-                  <div className="h-[300px] overflow-y-auto">
+                  <div className="h-[300px] overflow-y-auto bg-[#f2f7fa]">
                     <Articles />
                   </div>
                 </div>
@@ -59,7 +59,94 @@ export default function IndexHomePage() {
           </div>
         </Col>
         <Col xs={24} md={4}>
-          2
+          <div className="h-[160px] overflow-y-auto bg-[#f2f7fa]">
+            <div className="bg-[#2573B5] px-3 py-1">
+              <Typography.Text className="text-sm font-medium !text-white uppercase">
+                Thông báo
+              </Typography.Text>
+            </div>
+
+            <div className="p-2">
+              <ul className="!list-disc">
+                <li className="pb-2">
+                  <a href="" className="text-xs">
+                    Tỷ giá hạch toán ngoại tệ tháng 08/2024
+                  </a>
+                </li>
+                <li className="pb-2">
+                  <a href="" className="text-xs">
+                    Ban Quản lý dự án đầu tư hệ thống mạng Lan cơ quan KBNN đề
+                    nghị cung cấp hồ sơ năng lực và báo giá(30/07/2024)
+                  </a>
+                </li>
+                <li className="pb-2">
+                  <a href="" className="text-xs">
+                    KBNN Quãng Ngãi thông báo xét tuyển hợp đồng lao động năm
+                    2024(31/07/2024)
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <Divider className="!my-1" />
+
+          <div className="h-[160px] overflow-y-auto bg-[#f2f7fa]">
+            <div className="bg-[#2573B5] px-3 py-1">
+              <Typography.Text className="text-sm font-medium !text-white uppercase">
+                Văn bản mới
+              </Typography.Text>
+            </div>
+
+            <div className="p-2">
+              <ul className="!list-disc">
+                <li className="pb-2">
+                  <a
+                    className="text-xs"
+                    href="https://vst.mof.gov.vn/webcenter/portal/kbnn/r/m/htvb/ct_vbm?id=8037&_adf.ctrl-state=ab06t037g_1&_afrLoop=47978032979645810#!%40%40%3F_afrLoop%3D47978032979645810%26id%3D8037%26_adf.ctrl-state%3Dab06t037g_5"
+                  >
+                    V/v Báo cáo tình hình thực hiện công khai dự toán
+                    2023(28/04/2023)
+                  </a>
+                </li>
+                <li className="pb-2">
+                  <a className="text-xs" href="">
+                    Vv phân công thực hiện các nhiệm vụ, đề án triển khai Chương
+                    trình hành động của Bộ Tài chính ...(01/12/2022)
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </Col>
+      </Row>
+
+      <Divider className="!my-3" />
+
+      <Row gutter={8}>
+        <Col span={6}>
+          <div
+            className="h-[70px] w-full bg-transparent bg-center bg-cover bg-no-repeat"
+            style={{ backgroundImage: `url(/static/tthc.png)` }}
+          />
+        </Col>
+        <Col span={6}>
+          <div
+            className="h-[70px] w-full bg-transparent bg-center bg-cover bg-no-repeat"
+            style={{ backgroundImage: `url(/static/dvc.png)` }}
+          />
+        </Col>
+        <Col span={6}>
+          <div
+            className="h-[70px] w-full bg-transparent bg-center bg-cover bg-no-repeat"
+            style={{ backgroundImage: `url(/static/nv.png)` }}
+          />
+        </Col>
+        <Col span={6}>
+          <div
+            className="h-[70px] w-full bg-transparent bg-center bg-cover bg-no-repeat"
+            style={{ backgroundImage: `url(/static/covid.png)` }}
+          />
         </Col>
       </Row>
     </div>
