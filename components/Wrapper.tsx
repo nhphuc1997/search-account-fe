0ac959e04e5df20f9af2c43b6faa8d42d7d93542
phucnh17 <phucnh17@msb.com.vn>
2024-08-02 20:35:49 +0700

    feat: init react query

--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -1,11 +1,10 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Button, Col, Divider, Layout, Row, Typography } from "antd";
+import { Button, Col, Divider, Layout, Row, Typography, Image } from "antd";
 import { Content, Footer, Header } from "antd/es/layout/layout";
 import BgBanner from "@/assets/bg_banner.jpg";
 import { useRouter } from "next/navigation";
 import { SearchOutlined } from "@ant-design/icons";
-import Link from "next/link";
 
 const queryClient = new QueryClient();
 
@@ -74,8 +73,42 @@ export default function Wrapper({
           </div>
         </Content>
 
-        <Footer className="!bg-white flex justify-center items-center">
-          2024
+        <Footer className="!bg-white w-full">
+          <div className="flex justify-center items-center">
+            <Typography.Text className="!text-[#246097] font-bold">
+              CỔNG THÔNG TIN ĐIỆN TỬ KHO BẠC NHÀ NƯỚC
+            </Typography.Text>
+          </div>
+
+          <div className="flex justify-between items-center py-3">
+            <div>
+              <Typography.Paragraph className="!my-1">
+                Giấy phép số 173/GP-TTĐT ngày 15/12/2022
+              </Typography.Paragraph>
+              <Typography.Paragraph className="!my-1">
+                Người chịu trách nhiệm: Bà Trần Thị Huệ
+              </Typography.Paragraph>
+              <Typography.Paragraph className="!my-1">
+                Phó Tổng Giám đốc Kho bạc Nhà nước – Trưởng Ban biên tập
+              </Typography.Paragraph>
+            </div>
+
+            <div>
+              <Image src="/static/handle_cert.png" preview={false} />
+            </div>
+
+            <div>
+              <Typography.Paragraph className="!my-1">
+                Số 32 Cát Linh - Đống Đa - Hà Nội
+              </Typography.Paragraph>
+              <Typography.Paragraph className="!my-1">
+                Tel: (84-24) 62 764 300 Fax: (84-24) 62 764 367
+              </Typography.Paragraph>
+              <Typography.Paragraph className="!my-1">
+                Email: congttdtkbnn@vst.gov.vn
+              </Typography.Paragraph>
+            </div>
+          </div>
         </Footer>
       </Layout>
     </QueryClientProvider>
